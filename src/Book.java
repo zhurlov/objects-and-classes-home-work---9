49abba99a319ae0ef0ca1a18f43995ae3c680267
Станислав <zurlov.s.s@outlook.com>
2022-10-27 21:31:00 +0300

    Домашняя работа №9. Объекты и классы.

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+String name;
+int publication;
+
+    public static Book Book(String name, int publication) {
+        Book book = new Book();
+        book.name = name;
+        book. publication = publication;
+        return book;
+
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
 public class Main {
+
+ //  public static Book toWriteBook(String name, int publication) {
+   //     Book book = new Book();
+     //   book.name = name;
+       // book. publication = publication;
+        //return book;
+
+    //}
+
+
+
     public static void main(String[] args) {
+        String name = "gregory";
+        int publication = 1988;
 
 
+
+        Book grigory = Book.Book("gregory",1988);
+        //grigory.name = "grigory";
+        System.out.println("grigory.name = " + grigory.name);
+        System.out.println(grigory.name);
+        //grigory.publication = 1988;
+        System.out.println("grigory.publication = " + grigory.publication);
 
 
 
     }
+
+
 }
